04bd97c35e8ca823d5a09293dcbfc13d61680937
Wen Hui Leng <wleng@andrew.cmu.edu>
2024-12-14 17:00:29 -0500

    edited coco and ocr codes

--- ocr/ocr_v3.py
+++ ocr/ocr_v3.py
@@ -2,27 +2,20 @@ import pytesseract
 from PIL import Image
 import cv2
 import numpy as np
-# img_path = "raw_imgs/tilt_stop_new.png"
-# img_path = "sign_dataset/train/4.jpg"
-img_path = "raw_imgs/stop2.jpg"
-# img_path = "raw_imgs/speed.png"
-# img_path = "raw_imgs/road_sign.png"
+
+img_path = "raw_imgs/stop_crop_2.png"
+
 img_color = cv2.imread(img_path)
 img = cv2.cvtColor(img_color, cv2.COLOR_BGR2GRAY)
-# img = cv2.resize(img, None, fx=2, fy=2)
 
-# Otsu Tresholding to find best threshold value
-# This converts the image into a binary one
 _, binary_image = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
 cv2.imwrite("processed_imgs/test_img.png", binary_image)
 
-# Perform pytesseract on the image
-results = pytesseract.image_to_string(img_color, config='--psm 6')
+results = pytesseract.image_to_string(binary_image, config='--psm 6')
 
 print("Text detected:")
 print(results)
 
-# Split results into lines
 lines = results.splitlines()
 print(lines)
 header = lines[0] 
@@ -37,12 +30,10 @@ for line in data:
         if text.strip(): 
             detected_text.append(text)
 
-# Drawing bounding boxes
-h, w, _ = img_color.shape  # Get the dimensions of the original image
+h, w, _ = img_color.shape  
 
-# Perform OCR to get word-level data
 processed_image = img_color.copy()
-boxes = pytesseract.image_to_boxes(processed_image,  config='--psm 6')
+boxes = pytesseract.image_to_boxes(binary_image,  config='--psm 6')
 
 for b in boxes.splitlines():
     b = b.split(" ")
@@ -54,11 +45,8 @@ for b in boxes.splitlines():
 cv2.imwrite("processed_imgs/output_with_letter_boxes.png", processed_image)
 
 
-# Finding the particular words
 word_image = img_color.copy()
-# Perform OCR with word-level data
 data = pytesseract.image_to_data(word_image, config='--psm 6', output_type=pytesseract.Output.DICT)
-# Draw bounding boxes around detected words
 n_boxes = len(data['text'])
 for i in range(n_boxes):
     word = data['text'][i].strip()
@@ -69,5 +57,4 @@ for i in range(n_boxes):
         cv2.rectangle(word_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
         cv2.putText(word_image, word, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
 
-# Save the annotated image
 cv2.imwrite("processed_imgs/output_with_word_boxes.png", word_image)
