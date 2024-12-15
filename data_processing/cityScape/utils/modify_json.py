e2f20d3b4def6cf3273d8a5e7f851757c371ab57
lge0322 <58399339+lge0322@users.noreply.github.com>
2024-12-15 22:08:35 +0900

    Update modify_json.py

--- data_processing/cityScape/utils/modify_json.py
+++ data_processing/cityScape/utils/modify_json.py
@@ -5,6 +5,7 @@ from cityscapesscripts.helpers.annotation import Annotation
 from cityscapesscripts.helpers.csHelpers import printError
 from cityscapesscripts.preparation.json2labelImg import createLabelImage
 
+# Reference: https://github.com/mcordts/cityscapesScripts/blob/master/cityscapesscripts/preparation/json2labelImg.py
 # testing
 inJson = "/Users/gaeunlee/Desktop/F2024/18794/Project/Cityscape_data/dataset/gtFine/train/aachen/aachen_000000_000019_gtFine_polygons.json"
 annotation = Annotation()
