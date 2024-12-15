bee562cd5da0e190dc96b7aee6433ed5d52214cd
lge0322 <58399339+lge0322@users.noreply.github.com>
2024-12-15 22:19:56 +0900

    Update modify_json.py

--- data_processing/cityScape/utils/modify_json.py
+++ data_processing/cityScape/utils/modify_json.py
@@ -1,12 +1,9 @@
 import json, os, glob
-
-# cityscapes imports
 from cityscapesscripts.helpers.annotation import Annotation
 from cityscapesscripts.helpers.csHelpers import printError
 from cityscapesscripts.preparation.json2labelImg import createLabelImage
 
 # Reference: https://github.com/mcordts/cityscapesScripts/blob/master/cityscapesscripts/preparation/json2labelImg.py
-# testing
 inJson = "/Users/gaeunlee/Desktop/F2024/18794/Project/Cityscape_data/dataset/gtFine/train/aachen/aachen_000000_000019_gtFine_polygons.json"
 annotation = Annotation()
 annotation.fromJsonFile(inJson)
@@ -20,7 +17,7 @@ def main():
         cityscapesPath = os.environ['CITYSCAPES_DATASET']
     else:
         cityscapesPath = os.path.join(os.path.dirname(os.path.realpath(__file__)),'..','..')
-    # how to search for all ground truth
+
     searchFine   = os.path.join( cityscapesPath , "gtFine"   , "*" , "*" , "*_gt*_polygons.json" )
     searchCoarse = os.path.join( cityscapesPath , "gtCoarse" , "*" , "*" , "*_gt*_polygons.json" )
 
@@ -30,19 +27,14 @@ def main():
     filesCoarse = glob.glob( searchCoarse )
     filesCoarse.sort()
 
-    # concatenate fine and coarse
     files = filesFine + filesCoarse
 
-    # quit if we did not find anything
     if not files:
         printError( "Did not find any files. Please consult the README." )
 
     print("Processing {} annotation files".format(len(files)))
 
-    # Define the labels to keep as traffic signs
     traffic_sign_label = "traffic sign"  # Add more labels if necessary
-
-    # Iterate through files
     progress = 0
     print("Progress: {:>3} %".format(progress * 100 / len(files)), end=' ')
     
@@ -64,14 +56,11 @@ def main():
                     "polygon": obj["polygon"]
                 })
 
-        # Update the object array
         data["objects"] = objects_arr
 
-        # Save the modified JSON back to the same file
         with open(f, 'w') as file:
             json.dump(data, file, indent=4)
 
-        # Update progress
         progress += 1
         print("\rProgress: {:>3} %".format(progress * 100 / len(files)), end=' ')
     
