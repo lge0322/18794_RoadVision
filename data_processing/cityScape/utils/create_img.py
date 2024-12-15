3dfaa3685c0b83154f626f5f20193bf05b9a5b36
lge0322 <58399339+lge0322@users.noreply.github.com>
2024-12-15 22:19:22 +0900

    Update create_img.py

--- data_processing/cityScape/utils/create_img.py
+++ data_processing/cityScape/utils/create_img.py
@@ -1,10 +1,8 @@
-# cityscapes imports
 from cityscapesscripts.helpers.annotation import Annotation
 from cityscapesscripts.helpers.csHelpers import printError
 from cityscapesscripts.preparation.json2labelImg import createLabelImage
 
 # testing
-# change line 8 and 12 to store in your local machine
 inJson = "/Users/gaeunlee/Desktop/F2024/18794/Project/Cityscape_data/dataset/gtFine/train/aachen/aachen_000000_000019_gtFine_polygons.json"
 annotation = Annotation()
 annotation.fromJsonFile(inJson)
